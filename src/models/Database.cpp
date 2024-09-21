6b941704dbd0397375a4ff7df0288bde5433d57d
DinaTolotra <RatsinjomananaTolotra@gmail.com>
2024-09-21 08:42:27 +0300

    Class: defining Database

--- src/models/Database.cpp
+++ src/models/Database.cpp
@@ -1,5 +1,63 @@
 #include "Database.hpp"
 
-Database::Database(QObject *parent)
-    : QObject{parent}
+// setting static propriety default value
+QSqlDatabase *Database::_db = nullptr;
+
+
+Database::Database()
+    : _query(nullptr)
 {}
+
+
+bool Database::setupConnection()
+{
+    if (Database::_db != nullptr) return true;
+
+    Database::_db = new QSqlDatabase();
+    *Database::_db = QSqlDatabase::addDatabase("QMYSQL");
+    Database::_db->setDatabaseName("projet_fin_annee");
+    Database::_db->setHostName("localhost");
+    return Database::_db->open("root", "root");
+}
+
+
+void Database::setupQuery()
+{
+    if (_query) return;
+    _query = new QSqlQuery(*Database::_db);
+}
+
+
+void Database::disconnect()
+{
+    if (_query != nullptr) {
+        _query->clear();
+        delete _query;
+    }
+    if (Database::_db != nullptr) {
+        Database::_db->close();
+        delete Database::_db;
+    }
+}
+
+
+bool Database::exec(QString sttm)
+{
+    _query->clear();
+    return _query->exec(sttm);
+}
+
+
+QVariant Database::value(QString colName)
+{
+    if (!_query) return QVariant();
+    if (!_query->isActive()) return QVariant();
+    return _query->value(colName);
+}
+
+
+bool Database::next()
+{
+    if (!_query) return false;
+    return _query->next();
+}
--- src/models/Database.hpp
+++ src/models/Database.hpp
@@ -1,15 +1,63 @@
 #ifndef DATABASE_HPP
 #define DATABASE_HPP
 
-#include <QObject>
+// Standard library
+#include <QtCore/QString>
+#include <QtCore/QVariant>
+#include <QtCore/QVector>
+#include <QtCore/QDebug>
 
-class Database : public QObject
+// Database library
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+
+
+class Database
 {
-    Q_OBJECT
 public:
-    explicit Database(QObject *parent = nullptr);
+    /**
+     * @brief Constructeur par defaut
+     */
+    Database();
 
-signals:
+    /**
+     * @brief Execute un commande sql
+     * @param sttm Le commande à exectuer
+     * @return True si l'execution s'est bien passée, False sinon
+     */
+    bool exec(QString sttm);
+    /**
+     * @brief Obtenir la valeur d'un colonne donnée
+     * @param colName
+     * @return
+     */
+    QVariant value(QString colName);
+    /**
+     * @brief Passe à l'enregitrement suivant
+     * @return True si il y a encore un ou plusieur enregistrements
+     */
+    bool next();
+
+    /**
+     * @brief Initialise le connection avec le DB
+     * @return
+     */
+    bool setupConnection();
+    /**
+     * @brief Initialise l'objet query
+     */
+    void setupQuery();
+
+    /**
+     * @brief Termine la connection
+     */
+    void disconnect();
+
+private:
+    // the _db instance follow the singleton pattern
+    static QSqlDatabase *_db;
+    QSqlQuery *_query;
 };
+
 
 #endif // DATABASE_HPP
