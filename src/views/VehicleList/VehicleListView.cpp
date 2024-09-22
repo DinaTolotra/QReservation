e582d4d009e3a3eb65ae2024a456f722aac3cba0
DinaTolotra <RatsinjomananaTolotra@gmail.com>
2024-09-22 12:16:40 +0300

    Handle user choice in vehicle list view

--- src/views/VehicleList/VehicleListView.cpp
+++ src/views/VehicleList/VehicleListView.cpp
@@ -6,9 +6,32 @@ VehicleListView::VehicleListView(QWidget *parent)
     , ui(new Ui::VehicleListView)
 {
     ui->setupUi(this);
+    setUserChoiceHandler();
 }
 
 VehicleListView::~VehicleListView()
 {
     delete ui;
+}
+
+
+void VehicleListView::setUserChoiceHandler()
+{
+    connect(ui->vehicleTable, &QTableWidget::cellDoubleClicked,
+            this, &VehicleListView::handleVehicleChoice);
+}
+
+
+qint32 VehicleListView::getRowVehicleNum(qint32 row)
+{
+    QTableWidgetItem *cellWgt = ui->vehicleTable->itemAt(0, row);
+    return cellWgt->text().toInt();
+}
+
+
+void VehicleListView::handleVehicleChoice(qint32 row, qint32 col)
+{
+    qint32 numveh = getRowVehicleNum(row);
+    if (!vehicleList.contains(numveh)) return;
+    emit vehicleChoosed(vehicleList[numveh]);
 }
--- src/views/VehicleList/VehicleListView.hpp
+++ src/views/VehicleList/VehicleListView.hpp
@@ -2,6 +2,11 @@
 #define VEHICLELISTVIEW_HPP
 
 #include <QGroupBox>
+#include <QLabel>
+#include <QMap>
+
+#include "src/models/Vehicle/Vehicle.hpp"
+
 
 namespace Ui {
 class VehicleListView;
@@ -16,7 +21,19 @@ public:
     ~VehicleListView();
 
 private:
+    void setUserChoiceHandler();
+
+    qint32 getRowVehicleNum(qint32 row);
+
+signals:
+    void vehicleChoosed(Vehicle veh);
+
+private slots:
+    void handleVehicleChoice(qint32 row, qint32 col);
+
+private:
     Ui::VehicleListView *ui;
+    QMap<qint32, Vehicle> vehicleList;
 };
 
 #endif // VEHICLELISTVIEW_HPP
