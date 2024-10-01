081dc8cd8c83664aeebbbe16e87126005bbf6e28
DinaTolotra <RatsinjomananaTolotra@gmail.com>
2024-10-01 14:37:54 +0300

    Finalization first realise

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,14 @@
 #include <QApplication>
-#include "src/views/Login/Login.hpp"
 #include "src/controllers/MainController.hpp"
-
-
-bool loginUser();
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    if (loginUser()) {
-        MainController engine;
-        engine.exec();
-        a.exec();
-    }
+    MainController engine;
+    engine.exec();
+    a.exec();
 
     return 0;
-}
-
-
-bool loginUser()
-{
-    Login login;
-    int retCode = login.exec();
-    return retCode == QDialog::Accepted;
 }
